281eac7871aac38131671715bfd1376971db55de
brayan0428 <brayan0428@gmail.com>
2018-05-26 15:40:52 -0500

    Version Final

--- app/src/main/java/brayan0428/appstudent/com/loginsp/LoginActivity.java
+++ app/src/main/java/brayan0428/appstudent/com/loginsp/LoginActivity.java
@@ -1,11 +1,13 @@
 package brayan0428.appstudent.com.loginsp;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toolbar;
+
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     Toolbar toolbar;
@@ -24,6 +26,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnVerde.setOnClickListener(this);
         btnRojo.setOnClickListener(this);
         btnAmarillo.setOnClickListener(this);
+
+        toolbar.setTitle("Ejemplo SharedPreferences");
+        if(obtenerColor() != getResources().getColor(R.color.colorPrimaryDark)){
+            toolbar.setBackgroundColor(obtenerColor());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().setStatusBarColor(obtenerColor());
+            }
+        }
     }
 
 
@@ -32,12 +42,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()){
             case R.id.btnVerde :
                 toolbar.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.colorGreen));
+                }
+                guardarColor(getResources().getColor(R.color.colorGreen));
                 break;
             case R.id.btnRojo :
                 toolbar.setBackgroundColor(getResources().getColor(R.color.colorRed));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.colorRed));
+                }
+                guardarColor(getResources().getColor(R.color.colorRed));
                 break;
             case R.id.btnAmarillo :
                 toolbar.setBackgroundColor(getResources().getColor(R.color.colorYellow));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.colorYellow));
+                }
+                guardarColor(getResources().getColor(R.color.colorYellow));
                 break;
         }
     }
